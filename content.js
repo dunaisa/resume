1e5b8012e9c2de53af4cadad46d01fc72cbf32b1
Elizabeth <dun1isa@yandex.ru>
2023-07-29 15:02:45 +0300

    fix: links, description

--- content.js
+++ content.js
@@ -11,9 +11,13 @@ const contentObj = {
     "ru": "Обо мне",
     "en": "About me"
   },
+  // "about-me-description": {
+  //   "ru": "Обладаю такими качествами как коммуникабельность, высокая степень ответственности, умение находить компромиссы с людьми, отзывчивость, самостоятельность, оперативность, пунктуальность. Готова прикладывать все усилия, чтобы быть полезным звеном организации.",
+  //   "en": "I have such qualities as communication skills, a high degree of responsibility, the ability to find compromises with people, responsiveness, independence, efficiency, punctuality. Extremely motivated for career change goal."
+  // },
   "about-me-description": {
-    "ru": "Обладаю такими качествами как коммуникабельность, высокая степень ответственности, умение находить компромиссы с людьми, отзывчивость, самостоятельность, оперативность, пунктуальность. Готова прикладывать все усилия, чтобы быть полезным звеном организации.",
-    "en": "I have such qualities as communication skills, a high degree of responsibility, the ability to find compromises with people, responsiveness, independence, efficiency, punctuality. Extremely motivated for career change goal."
+    "ru": "Мне нравится пробовать что-то новое и развиваться. Так, через пробы и ошибки я пришла к тому, чему готова посвятить всю жизнь - фронтенд разработке. Я достаточно организованный, самостоятельный и ответственный человек. Стремлюсь выходить на новый уровнь, но не горю идеей занимать руководящие должности, так как я хороший исполнитель и мне комфортно в этой роли. Цель: чувствовать, что я приношу пользу организации, реализовываться, будучи частью комьюнити.",
+    "en": "I like to try something new and develop. So, through trial and error, I came to what I am ready to devote my whole life to - frontend development. I am quite organized, independent and responsible person. I strive to go to the next level, but I am not looking for a leadership position, as I am a good performer and I am comfortable in this role. Му goal is to feel that I benefit the organization, to be realized by being part of the community."
   },
   "interests-title": {
     "ru": "Интересы",
@@ -48,23 +52,51 @@ const contentObj = {
     "en": "Education"
   },
   "edu-university": {
-    "ru": "РГСУ, Экономический факультет, 2019 год",
-    "en": "RUOS"
+    "ru": "РГСУ, Экономический факультет, 2019",
+    "en": "RUOS, Economics faculty, 2019"
   },
   "edu-course-yandex": {
-    "ru": "Yandex prakticum, Веб-разработчик, 2022 год",
-    "en": "Yandex prakticum, Frontend Developer, 2022"
+    "ru": "Yandex practicum, Веб-разработчик, 2022",
+    "en": "Yandex practicum, Web Developer, 2022"
   },
   "experience-title": {
     "ru": "Опыт работы",
     "en": "Experience"
   },
   "experience-info": {
-    "ru": "Релевантного официального опыта работы не было. Немного фриланс.",
-    "en": "No"
+    "ru": "Коммерческого опыта работы не было",
+    "en": "No commercial experience"
   },
   "skills-title": {
     "ru": "Навыки",
     "en": "Skills"
+  },
+  "portfolio-title": {
+    "ru": "Портфолио",
+    "en": "Portfolio"
+  },
+  "landing-rus-trip": {
+    "ru": "Лендинг Путешествие по России",
+    "en": "Landing Trip Around Russia"
+  },
+  "quiz": {
+    "ru": "Квиз по странам Европы (десктопный)",
+    "en": "Quiz (desktop)"
+  },
+  "meta-network": {
+    "ru": "Квиз по странам Европы",
+    "en": "Social network (Analogue of Meta)"
+  },
+  "planner": {
+    "ru": "Планнер",
+    "en": "Planner"
+  },
+  "part-one": {
+    "ru": " Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной",
+    "en": " keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch "
+  },
+  "part-two": {
+    "ru": "Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной Свяжитесь со мной",
+    "en": " keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch keep in touch "
   }
 }
